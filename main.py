0c2b394d1c9660ad3181e84249bb7968abd043fe
Jonh <jonhblvm@outlook.com>
2021-04-24 12:17:44 -0300

    main.py modificado
    parte dos prints removidos

--- main.py
+++ main.py
@@ -3,13 +3,4 @@ from calculadora import somar, subtrair, multiplicar, dividir, calc_all, rad, po
 a = int(input("Insira o número a:"))
 b = int(input("Insira o número b:"))
 
-"""print(f'Soma {a} e {b}: {somar(a, b)}')
-print(f'Subtração {a} e {b}: {subtrair(a, b)}')
-print(f'Multiplicação {a} e {b}: {multiplicar(a, b)}')
-print(f'Divisão {a} e {b}: {dividir(a, b):.3f}') 
-
-calc_all(a,b)"""
-
 print(rad(a,b))
-print(pot(a,b))
-print(pot(a,b)*rad(a,b))

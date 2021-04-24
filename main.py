ecf81800608e02f0a7b536f495b85506b13cbf7a
Jonh <jonhblvm@outlook.com>
2021-04-24 11:18:09 -0300

    Função de radiciação pronta

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from calculadora import somar, subtrair, multiplicar, dividir, calc_all
+from calculadora import somar, subtrair, multiplicar, dividir, calc_all, rad
 
 a = int(input("Insira o número a:"))
 b = int(input("Insira o número b:"))
 
-print(f'Soma {a} e {b}: {somar(a, b)}')
+"""print(f'Soma {a} e {b}: {somar(a, b)}')
 print(f'Subtração {a} e {b}: {subtrair(a, b)}')
 print(f'Multiplicação {a} e {b}: {multiplicar(a, b)}')
 print(f'Divisão {a} e {b}: {dividir(a, b):.3f}') 
 
-calc_all(a,b)
+calc_all(a,b)"""
+
+print(rad(a,b))

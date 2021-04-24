754b65bc67af94d2796346ede280b1f51001e860
Jonh <jonhblvm@outlook.com>
2021-04-24 10:57:39 -0300

    Função calc_all pronta

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from calculadora import somar, subtrair, multiplicar, dividir
+from calculadora import somar, subtrair, multiplicar, dividir, calc_all
 
 a = int(input("Insira o número a:"))
 b = int(input("Insira o número b:"))
@@ -7,3 +7,5 @@ print(f'Soma {a} e {b}: {somar(a, b)}')
 print(f'Subtração {a} e {b}: {subtrair(a, b)}')
 print(f'Multiplicação {a} e {b}: {multiplicar(a, b)}')
 print(f'Divisão {a} e {b}: {dividir(a, b):.3f}') 
+
+calc_all(a,b)

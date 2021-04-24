8c89a6bbc0ced73b78d843d8bf44e85a82c6260c
Jonh <jonhblvm@outlook.com>
2021-04-24 11:58:40 -0300

    Modulo finalizado

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ b = int(input("Insira o número b:"))
 """print(f'Soma {a} e {b}: {somar(a, b)}')
 print(f'Subtração {a} e {b}: {subtrair(a, b)}')
 print(f'Multiplicação {a} e {b}: {multiplicar(a, b)}')
-print(f'Divisão {a} e {b}: {dividir(a, b):.3f}') 
+print(f'Divisão {a} e {b}: {dividir(a, b):.3f}') """
 
-calc_all(a,b)"""
-
+calc_all(a,b)
 print(rad(a,b))
 print(pot(a,b))

a375ce3d6e41ff975668b78c71d532741fa2eb5e
Jonh <jonhblvm@outlook.com>
2021-04-24 10:17:35 -0300

    Funções da calculadora modificadas.

--- calculadora.py
+++ calculadora.py
@@ -1,11 +1,11 @@
 def somar(a,b):
-    return a+b
+    return a/b
 
 def subtrair(a,b):
-    return a-b
+    return a+b
 
 def multiplicar(a,b):
-    return a*b
+    return a-b
 
 def dividir(a,b):
-    return a/b
+    return a*b

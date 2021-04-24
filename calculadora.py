9710f2331d0338cabaf2a316973e941fc7a8c71d
Jonh <jonhblvm@outlook.com>
2021-04-24 10:52:48 -0300

    Função responsável por retornar todas operações básicas

--- calculadora.py
+++ calculadora.py
@@ -9,3 +9,9 @@ def multiplicar(a,b):
 
 def dividir(a,b):
     return a/b
+
+def calc_all(a,b):
+    print(somar(a,b))
+    print(subtrair(a,b))
+    print(multiplicar(a,b))
+    print(dividir(a,b))
